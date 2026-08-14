d354a1400796fc760a66c8481e4d6c411ebd0d0e
Ethan Stokes <erstokes10@gmail.com>
2026-08-14 10:14:09 -0500

    ML gradle

---
+++ neoforge/src/main/java/net/frozenblock/glowtone/GlowtoneNeoForge.java
@@ -0,0 +1,13 @@
+package net.frozenblock.glowtone;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(GlowtoneNeoForge.MOD_ID)
+public final class GlowtoneNeoForge {
+	//todo temp
+	public static final String MOD_ID = "glowtone";
+
+	public GlowtoneNeoForge(IEventBus modBus) {
+	}
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,95 @@
+import com.possible_triangle.gradle.settings.ResolutionStrategy
+
 pluginManagement {
     repositories {
+        mavenLocal()
         maven("https://maven.quiltmc.org/repository/release") {
             name = "Quilt"
         }
         maven("https://maven.fabricmc.net") {
             name = "Fabric"
         }
+        maven("https://maven.neoforged.net/releases") {
+            name = "NeoForged"
+        }
         maven("https://jitpack.io") {
             name = "Jitpack"
+        }
+        maven("https://registry.somethingcatchy.net/repository/maven-releases/") { // Candlelight & Triangle
+            name = "SomethingCatchy (MehVahdJukaar)"
+        }
+        maven("https://maven.frozenblock.net/snapshot") {
+            name = "FrozenBlock Snapshot"
         }
         mavenCentral()
         gradlePluginPortal()
     }
 }
 
+val neoforgeSnapshotMaven = settings.providers.gradleProperty("neoforge_snapshot_maven").orNull
+if (!neoforgeSnapshotMaven.isNullOrBlank()) {
+    pluginManagement {
+        repositories {
+            maven(neoforgeSnapshotMaven) { name = "NeoForge Snapshots" }
+        }
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("+")
+    id("net.frozenblock.triangle.helper") version("+")
+}
+
+helper {
+    versionStrategy = ResolutionStrategy.SNAPSHOT
+}
+
 rootProject.name = "Glowtone"
+
+object Constants {
+    const val FABRIC: Boolean = true
+    const val NEOFORGE: Boolean = true
+}
+
+include("gt-common")
+project(":gt-common").projectDir = file("common")
+
+if (Constants.FABRIC) {
+    include("gt-fabric")
+    project(":gt-fabric").projectDir = file("fabric")
+}
+
+if (Constants.NEOFORGE) {
+    include("gt-neoforge")
+    project(":gt-neoforge").projectDir = file("neoforge")
+}
+
+localPluginRepository(
+    "GradleHelper",
+    enabled = true
+)
+
+fun localPluginRepository(repo: String, enabled: Boolean = true) {
+    if (!enabled) return
+    println("Attempting to include local plugin build $repo")
+
+    val github = System.getenv("GITHUB_ACTIONS") == "true"
+
+    var path = "../$repo"
+    var file = File(path)
+
+    if (github) {
+        path = repo
+        file = File(path)
+        println("Running on GitHub")
+    }
+
+    if (file.exists()) {
+        pluginManagement {
+            includeBuild(path)
+        }
+        println("Included local plugin build $repo")
+    } else {
+        println("Local plugin build $repo not found")
+    }
+}
