d4a34c63c0e2563cfc8279b9def45bf2a3af1c63
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2026-08-20 12:03:20 -0400

    fix mixin config

--- fabric/src/main/java/net/frozenblock/glowtone/mixin/client/emissive/block/BlocksMixin.java
+++ fabric/src/main/java/net/frozenblock/glowtone/mixin/client/emissive/block/BlocksMixin.java
@@ -15,7 +15,7 @@
  * along with this program; if not, see <https://github.com/FrozenBlock/Licenses>.
  */
 
-package net.frozenblock.glowtone.mixin.client.block;
+package net.frozenblock.glowtone.mixin.client.emissive.block;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
